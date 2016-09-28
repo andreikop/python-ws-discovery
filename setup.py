05867dc4faeada7ef355b4d877a9a18da818a2f8
Pieter Jordaan <pieterwjordaanpc@gmail.com>
2016-09-28 11:22:58 +0200

    Restore original author details

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ from distutils.core import setup
 setup(name='WSDiscovery',
       version='0.2',
       description='WS-Discovery implementation for python',
-      author='Pieter Willem Jordaan',
-      author_email='pieterwjordaanpc@gmail.com',
-      url='https://github.com/Climax777/python-ws-discovery.git',
+      author='L.A. Fernando',
+      author_email='lafernando@gmail.com',
+      url='https://github.com/hlamer/python-ws-discovery.git',
       py_modules=['WSDiscovery']
      )
