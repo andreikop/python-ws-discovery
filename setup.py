47458308fdcf943af18d8bd41d564163d02a90e1
Pieter Jordaan <pieterwjordaanpc@gmail.com>
2016-09-28 15:03:50 +0200

    Added trove classifiers

--- setup.py
+++ setup.py
@@ -8,5 +8,20 @@ setup(name='WSDiscovery',
       author='L.A. Fernando',
       author_email='lafernando@gmail.com',
       url='https://github.com/hlamer/python-ws-discovery.git',
+      classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Console',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+            'Operating System :: MacOS :: MacOS X',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.3',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Topic :: Software Development :: Libraries',
+            'Topic :: Software Development :: Libraries :: Python Modules',
+            'Topic :: Communications'
+      ],
       py_modules=['WSDiscovery']
      )
