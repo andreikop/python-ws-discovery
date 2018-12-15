50afa8b2882d1758516e5be901fbd4ce98bfe41b
Petri Savolainen <petri@koodaamo.fi>
2018-12-15 20:52:03 +0200

    major refactoring

---
+++ wsdiscovery/uri.py
@@ -0,0 +1,39 @@
+
+"""
+Simple URI implementation, just for the purposes of service scope matching
+"""
+
+from urllib.parse import unquote
+
+
+class URI:
+
+    def __init__(self, uri):
+        uri = unquote(uri)
+        i1 = uri.find(":")
+        i2 = uri.find("@")
+        self._scheme = uri[:i1]
+        if i2 != -1:
+            self._authority = uri[i1 + 1: i2]
+            self._path = uri[i2 + 1:]
+        else:
+            self._authority = ""
+            self._path = uri[i1 + 1:]
+
+    def getScheme(self):
+        return self._scheme
+
+    def getAuthority(self):
+        return self._authority
+
+    def getPath(self):
+        return self._path
+
+    def getPathExQueryFragment(self):
+        i = self._path.find("?")
+        path = self.getPath()
+        if i != -1:
+            return path[:self._path.find("?")]
+        else:
+            return path
+
