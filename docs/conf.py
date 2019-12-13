7dd8dd6550fb479a3e0fc7c22e50602e5aca57a1
Petri Savolainen <petri@koodaamo.fi>
2019-12-13 20:06:08 +0200

    some Sphinx versions assume different master docs; specify it explicitly

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,8 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
+# Define the top-level master doc
+master_doc = 'index'
 
 # -- Options for HTML output -------------------------------------------------
 
